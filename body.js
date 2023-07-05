ddf040454b3168111abfd69d697f05a0ec4eaa09
Muhammad Iklilun Nafisi <121439535+Biasasajaa@users.noreply.github.com>
2023-07-05 19:42:43 +0700

    new

---
+++ body.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    $('#1').css('text-align', 'justify');
+    $('.cadet').css('background-color', 'cadetblue')
+    $('.light').css('background-color', 'lightseagreen');
+    $('.ok').css('font-size', '25px');
+
+    $('#a').mouseenter(function () {
+        $('#a').css('text-decoration', 'underline');
+    });
+    $('#a').mouseleave(function () {
+        $('#a').css('text-decoration', 'none');
+    });
+    $('#b').mouseenter(function () {
+        $('#b').css('text-decoration', 'underline');
+    });
+    $('#b').mouseleave(function () {
+        $('#b').css('text-decoration', 'none');
+    });
+    $('#c').mouseenter(function () {
+        $('#c').css('text-decoration', 'underline');
+    });
+    $('#c').mouseleave(function () {
+        $('#c').css('text-decoration', 'none');
+    });
+    $('#d').mouseenter(function () {
+        $('#d').css('text-decoration', 'underline');
+    });
+    $('#d').mouseleave(function () {
+        $('#d').css('text-decoration', 'none');
+    });
+    $('#e').mouseenter(function () {
+        $('#e').css('text-decoration', 'underline');
+    });
+    $('#e').mouseleave(function () {
+        $('#e').css('text-decoration', 'none');
+    });
+    $('#f').mouseenter(function () {
+        $('#f').css('text-decoration', 'underline');
+    });
+    $('#f').mouseleave(function () {
+        $('#f').css('text-decoration', 'none');
+    });
+});
